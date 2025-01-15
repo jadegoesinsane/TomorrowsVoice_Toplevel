e6efd9bbca9c80e036e9fd5ef6b752e8a4a7ddea
RawicePhil <rawicephil@gmail.com>
2025-01-15 16:27:57 -0500

    Create Director.cs

---
+++ Models/Director.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TomorrowsVoice_Toplevel.Models
+{
+    public class Director
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "You cannot leave the first name blank.")]
+        [MaxLength(50, ErrorMessage = "First name cannot be more than 50 characters long.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You cannot leave the last name blank.")]
+        [MaxLength(100, ErrorMessage = "Last name cannot be more than 100 characters long.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email address is required.")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Please follow the correct email format test@email.com")]
+        [StringLength(255)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = "";
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression("^\\d{10}$", ErrorMessage = "Please enter a valid 10-digit phone number (no spaces).")]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(10)]
+        public string Phone { get; set; } = "";
+
+        [Display(Name = "Chapter ID")]
+        [Required(ErrorMessage = "You must select the Chapter ID")]
+
+        public int ChapterID { get; set; }
+
+        public Chapter? Chapter { get; set; }
+    }
+}
