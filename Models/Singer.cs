dbb176fc3d60a8d2f9d56eef738ae4f159281c3c
jadegoesinsane <jkritzer1@ncstudents.niagaracollege.ca>
2025-01-15 17:07:25 -0500

    Fix Singer

--- Models/Singer.cs
+++ Models/Singer.cs
@@ -1,10 +1,9 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace WebApplication1.Models
+namespace TomorrowsVoice_Toplevel.Models
 {
-    public class Member
+    public class Singer
     {
-
         public int ID { get; set; }
 
         [Display(Name = "First Name")]
@@ -12,27 +11,23 @@ namespace WebApplication1.Models
         [MaxLength(50, ErrorMessage = "First name cannot be more than 50 characters long.")]
         public string FirstName { get; set; } = "";
 
-        
         [Display(Name = "Last Name")]
         [Required(ErrorMessage = "You cannot leave the last name blank.")]
         [MaxLength(100, ErrorMessage = "Last name cannot be more than 100 characters long.")]
         public string LastName { get; set; } = "";
 
-        [Display(Name = "EmergencyContact")]
+        [Display(Name = "Emergency Contact")]
         [Required(ErrorMessage = "You cannot leave the last name blank.")]
         [MaxLength(100, ErrorMessage = " Name cannot be more than 100 characters long.")]
         public string ContactName { get; set; } = "";
-
 
         [Display(Name = "Notes")]
         [StringLength(255)]
         [DataType(DataType.MultilineText)]
         public string? Note { get; set; }
-
         [Display(Name = "Chapter")]
         public int ChapterID { get; set; }
         public Chapter? Chapter { get; set; }
-
         public ICollection<RehearsalAttendance> RehearsalAttendances { get; set; } = new HashSet<RehearsalAttendance>();
     }
 }
