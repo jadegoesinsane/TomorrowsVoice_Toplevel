cbd16c86a2a61acdbb9a4081b5d744acc0b2d6bb
jadegoesinsane <jkritzer1@ncstudents.niagaracollege.ca>
2025-01-15 11:08:25 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TomorrowsVoice_Toplevel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
