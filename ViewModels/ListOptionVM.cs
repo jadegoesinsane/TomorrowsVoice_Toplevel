7b0bcd1c744b445dd4f9fd7ae845aa170d1456d4
cheng1111111111111 <tli15@ncstudents.niagaracollege.ca>
2025-01-18 10:03:21 -0500

    Create ListOptionVM.cs

---
+++ ViewModels/ListOptionVM.cs
@@ -0,0 +1,8 @@
+namespace TomorrowsVoice_Toplevel.ViewModels
+{
+    public class ListOptionVM
+    {
+        public int ID { get; set; }
+        public string DisplayText { get; set; } = "";
+    }
+}
