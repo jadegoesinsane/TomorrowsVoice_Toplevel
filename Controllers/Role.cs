af7906cc72794486ac9b907e6fee3c138b07de72
cheng1111111111111 <tli15@ncstudents.niagaracollege.ca>
2025-01-21 12:43:03 -0500

    add new person

---
+++ Controllers/Role.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TomorrowsVoice_Toplevel.Controllers
+{
+    public class Role : Controller
+    {
+        public IActionResult Index(string role)
+        {
+            if (string.IsNullOrEmpty(role))
+            {
+
+                return View();
+            }
+
+            if (role == "Singer")
+            {
+                return RedirectToAction("Create", "Singer");
+            }
+            else if (role == "Director")
+            {
+                return RedirectToAction("Create", "Director");
+            }
+
+            return View();
+        }
+
+    }
+}
