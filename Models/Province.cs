e526a0d05e98bc6577c322509d417d4644f2439f
cheng1111111111111 <tli15@ncstudents.niagaracollege.ca>
2025-01-24 07:33:13 -0500

    province

---
+++ Models/Province.cs
@@ -0,0 +1,19 @@
+﻿namespace TomorrowsVoice_Toplevel.Models
+{
+    public enum Province
+    {
+        Alberta,
+        BritishColumbia,
+        Manitoba,
+        NewBrunswick,
+        NewfoundlandAndLabrador,
+        NovaScotia,
+        Ontario,
+        PrinceEdwardIsland,
+        Quebec,
+        Saskatchewan,
+        NorthwestTerritories,
+        Nunavut,
+        Yukon
+    }
+}
