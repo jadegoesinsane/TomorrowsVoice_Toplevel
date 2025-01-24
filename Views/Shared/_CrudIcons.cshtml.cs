b096a74a2cd7b8652a7b34a47f98c713d82ad06f
jadegoesinsane <jkritzer1@ncstudents.niagaracollege.ca>
2025-01-24 11:59:25 -0500

    Add Icons Partial View This will be replaced in some way in the future as we have yet to add authentication; this was simply so we could quickly update how the icons were styled without changing it everywhere

---
+++ Views/Shared/_CrudIcons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TomorrowsVoice_Toplevel.Views.Shared
+{
+    public class _CrudIconsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
