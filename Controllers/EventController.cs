2524c9b04b970e818654067fc658102a8c1cd993
cheng1111111111111 <tli15@ncstudents.niagaracollege.ca>
2025-02-16 19:10:32 -0500

    event controller

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using NToastNotify;
 using TomorrowsVoice_Toplevel.CustomControllers;
 using TomorrowsVoice_Toplevel.Data;
+using TomorrowsVoice_Toplevel.Models;
 using TomorrowsVoice_Toplevel.Models.Volunteering;
 
 namespace TomorrowsVoice_Toplevel.Controllers
@@ -58,13 +60,35 @@ namespace TomorrowsVoice_Toplevel.Controllers
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Create([Bind("ID,Name,StartDate,EndDate,Descripion,Location,Status")] Event @event)
 		{
-			if (ModelState.IsValid)
-			{
-				_context.Add(@event);
-				await _context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			return View(@event);
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(@event);
+                    await _context.SaveChangesAsync();
+                    AddSuccessToast(@event.Name);
+                    //_toastNotification.AddSuccessToastMessage($"{singer.NameFormatted} was successfully created.");
+                    return RedirectToAction("Details", new { @event.ID });
+                }
+            }
+            catch (DbUpdateException dex)
+            {
+                string message = dex.GetBaseException().Message;
+                if (message.Contains("UNIQUE") && message.Contains("@event.????????"))
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Remember, " +
+                        "you cannot have duplicate Name and Email.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+            }
+
+
+            return View(@event);
+
+           
 		}
 
 		// GET: Event/Edit/5
@@ -88,35 +112,55 @@ namespace TomorrowsVoice_Toplevel.Controllers
 		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("ID,Name,StartDate,EndDate,Descripion,Location,Status")] Event @event)
+		public async Task<IActionResult> Edit(int id)
 		{
-			if (id != @event.ID)
-			{
-				return NotFound();
-			}
+            var @eventToUpdate = await _context.Events
 
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(@event);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!EventExists(@event.ID))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			return View(@event);
-		}
+               .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (@eventToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            // Try updating with posted values
+            if (await TryUpdateModelAsync<Event>(@eventToUpdate,
+                    "",
+					   r => r.Name,
+					   r => r.StartDate,
+					 r => r.EndDate,
+					 r => r.Location,
+                    r => r.Descripion,
+                    r => r.Status))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    _toastNotification.AddSuccessToastMessage($"{@eventToUpdate.Name} was successfully updated.");
+                    return RedirectToAction("Details", new { @eventToUpdate.ID });
+                }
+                catch (RetryLimitExceededException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes after multiple attempts. Please Try Again.");
+                }
+                catch (DbUpdateException dex)
+                {
+                    string message = dex.GetBaseException().Message;
+                    if (message.Contains("UNIQUE") && message.Contains("Volunteers.Email"))
+                    {
+                        ModelState.AddModelError("", "Unable to save changes. Remember, " +
+                            "you cannot have duplicate Name and Email.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                    }
+                }
+            }
+
+
+            return View(@eventToUpdate);
+        }
 
 		// GET: Event/Delete/5
 		public async Task<IActionResult> Delete(int? id)
@@ -141,15 +185,30 @@ namespace TomorrowsVoice_Toplevel.Controllers
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> DeleteConfirmed(int id)
 		{
-			var @event = await _context.Events.FindAsync(id);
-			if (@event != null)
-			{
-				_context.Events.Remove(@event);
-			}
+            var @event = await _context.Events
 
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
+               .FirstOrDefaultAsync(m => m.ID == id);
+
+            try
+            {
+                if (@event != null)
+                {
+                    //_context.Singers.Remove(singer);
+
+                    // Here we are archiving a singer instead of deleting them
+                    @event.Status = Status.Archived;
+                    await _context.SaveChangesAsync();
+                    AddSuccessToast(@event.Name);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to delete record. Please try again.");
+            }
+
+            return View(@event);
+        }
 
 		private bool EventExists(int id)
 		{
