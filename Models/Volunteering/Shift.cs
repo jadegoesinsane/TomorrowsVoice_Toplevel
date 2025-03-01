c831ea034a9b8f7f274f79638d4a922c4a0d2476
jadegoesinsane <jkritzer1@ncstudents.niagaracollege.ca>
2025-03-01 16:52:33 -0500

    shifttime

--- Models/Volunteering/Shift.cs
+++ Models/Volunteering/Shift.cs
@@ -5,60 +5,77 @@ using TomorrowsVoice_Toplevel.Models.Messaging;
 
 namespace TomorrowsVoice_Toplevel.Models.Volunteering
 {
-    public class Shift
-    {
-        public int ID { get; set; }
-		public String TimeFormat =>
-			StartAt.ToString("tt") == EndAt.ToString("tt")
-				? $"{StartAt:h:mm} - {EndAt:h:mm}{EndAt:tt}".ToLower()
-				: $"{StartAt:h:mm}{StartAt:tt} - {EndAt:h:mm}{EndAt:tt}".ToLower();
+	public class Shift
+	{
+		public int ID { get; set; }
+
+		public string TimeFormat
+		{
+			get
+			{
+				string start = StartAt.ToString(StartAt.Minute == 0 ? "%h" : "h:mm");
+				string end = EndAt.ToString(EndAt.Minute == 0 ? "%h" : "h:mm");
+				string startTT = StartAt.ToString("tt").ToLower();
+				string endTT = EndAt.ToString("tt").ToLower();
+
+				if (startTT == endTT && StartAt.Date == EndAt.Date)
+				{
+					return $"{start} - {end}{endTT}";
+				}
+				else
+				{
+					return $"{start}{startTT} - {end}{endTT}";
+				}
+			}
+		}
+
 		public String DateFormat => ShiftDate.ToString("dddd, MMMM d");
-        public String TimeSummary => $"{DateFormat} ⋅ {TimeFormat}";
+		public String TimeSummary => $"{DateFormat} ⋅ {TimeFormat}";
 
 		[Display(Name = "Date")]
-        [Required(ErrorMessage = "Please select a date for this Shift")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime ShiftDate { get; set; }
+		[Required(ErrorMessage = "Please select a date for this Shift")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime ShiftDate { get; set; }
 
-        [Required(ErrorMessage = "Shift requires a start time.")]
-        [Display(Name = "Start Time")]
-        [DataType(DataType.Time)]
-        public DateTime StartAt { get; set; }
+		[Required(ErrorMessage = "Shift requires a start time.")]
+		[Display(Name = "Start Time")]
+		[DataType(DataType.Time)]
+		public DateTime StartAt { get; set; }
 
-        [Required(ErrorMessage = "Shift requires an end time.")]
-        [Display(Name = "End Time")]
-        [DataType(DataType.Time)]
-        public DateTime EndAt { get; set; }
+		[Required(ErrorMessage = "Shift requires an end time.")]
+		[Display(Name = "End Time")]
+		[DataType(DataType.Time)]
+		public DateTime EndAt { get; set; }
 
-        [Display(Name = "Shift Duration")]
-        public TimeSpan ShiftDuration
-        {
-            get
-            {
-                return EndAt - StartAt;
-            }
-        }
+		[Display(Name = "Shift Duration")]
+		public TimeSpan ShiftDuration
+		{
+			get
+			{
+				return EndAt - StartAt;
+			}
+		}
 
-        [Display(Name = "Status")]
-        public Status Status { get; set; } = Status.Active;
+		[Display(Name = "Status")]
+		public Status Status { get; set; } = Status.Active;
 
-        [Display(Name = "Volunteers Needed")]
-        public int VolunteersNeeded { get; set; }
+		[Display(Name = "Volunteers Needed")]
+		public int VolunteersNeeded { get; set; }
 
-        public int VolunteersLeft
-        {
-            get
-            {
-                int count = VolunteersNeeded - UserShifts.Count();
-                if (count <= 0)
-                    return 0;
-                else
-                    return count;
-            }
-        }
+		public int VolunteersLeft
+		{
+			get
+			{
+				int count = VolunteersNeeded - UserShifts.Count();
+				if (count <= 0)
+					return 0;
+				else
+					return count;
+			}
+		}
 
-        /*public int VolunteersSignedUp
+		/*public int VolunteersSignedUp
 		{
 			get
 			{
@@ -70,28 +87,28 @@ namespace TomorrowsVoice_Toplevel.Models.Volunteering
 			}
 		}*/
 
-        public int EventID { get; set; }
-        public Event? Event { get; set; }
-        public virtual ICollection<UserShift> UserShifts { get; set; } = new HashSet<UserShift>();
+		public int EventID { get; set; }
+		public Event? Event { get; set; }
+		public virtual ICollection<UserShift> UserShifts { get; set; } = new HashSet<UserShift>();
 
-        public void AddChat(TVContext context)
-        {
-            if (!context.Chats.Any(d => d.ID == ID))
-            {
-                Chat chat = new Chat
-                {
-                    ID = ID,
-                    Shift = this,
-                    Title = $"Shift {ID}"
-                };
-                context.Chats.Add(chat);
-                context.SaveChanges();
-            }
-        }
+		public void AddChat(TVContext context)
+		{
+			if (!context.Chats.Any(d => d.ID == ID))
+			{
+				Chat chat = new Chat
+				{
+					ID = ID,
+					Shift = this,
+					Title = $"Shift {ID}"
+				};
+				context.Chats.Add(chat);
+				context.SaveChanges();
+			}
+		}
 
-        public static explicit operator Shift(Task<Shift?> v)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public static explicit operator Shift(Task<Shift?> v)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
