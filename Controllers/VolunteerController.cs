643cee2208e428788f3ed4b0f3b55ba3363624fb
cheng1111111111111 <tli15@ncstudents.niagaracollege.ca>
2025-03-07 10:31:24 -0500

    vo controller

--- Controllers/VolunteerController.cs
+++ Controllers/VolunteerController.cs
@@ -33,151 +33,7 @@ namespace TomorrowsVoice_Toplevel.Controllers
 		}
 
 
-		public async Task<IActionResult> IndexVolunteer(string? SearchString, int? page, int? pageSizeID, string? actionButton, string? StatusFilter, string sortField = "Volunteer", string sortDirection = "asc")
-		{
-			string[] sortOptions = new[] { "Volunteer", "Hours Volunteered", "Participation", "Absences" };
-
-			//Count the number of filters applied - start by assuming no filters
-			ViewData["Filtering"] = "btn-outline-secondary";
-			int numberFilters = 0;
-			Enum.TryParse(StatusFilter, out Status selectedDOW);
-
-
-			var statusList = Enum.GetValues(typeof(Status))
-						 .Cast<Status>()
-						 .Where(s => s == Status.Active || s == Status.Archived)
-						 .ToList();
-
-
-			ViewBag.StatusList = new SelectList(statusList);
-			var volunteers = _context.Volunteers.AsNoTracking();
-			if (!String.IsNullOrEmpty(StatusFilter))
-			{
-				volunteers = volunteers.Where(p => p.Status == selectedDOW);
-
-				// filter out archived singers if the user does not specifically select "archived"
-				if (selectedDOW != Status.Archived)
-				{
-					volunteers = volunteers.Where(s => s.Status != Status.Archived);
-				}
-				numberFilters++;
-			}
-			else
-			{
-				volunteers = volunteers.Where(s => s.Status != Status.Archived);
-			}
-			if (!String.IsNullOrEmpty(SearchString))
-			{
-				volunteers = volunteers.Where(p => p.LastName.ToUpper().Contains(SearchString.ToUpper())
-									   || p.FirstName.ToUpper().Contains(SearchString.ToUpper()));
-				numberFilters++;
-			}
-			//Give feedback about the state of the filters
-			if (numberFilters != 0)
-			{
-				//Toggle the Open/Closed state of the collapse depending on if we are filtering
-				ViewData["Filtering"] = " btn-danger";
-				//Show how many filters have been applied
-				ViewData["numberFilters"] = "(" + numberFilters.ToString()
-					+ " Filter" + (numberFilters > 1 ? "s" : "") + " Applied)";
-				//Keep the Bootstrap collapse open
-				@ViewData["ShowFilter"] = " show";
-			}
-			//Before we sort, see if we have called for a change of filtering or sorting
-			if (!String.IsNullOrEmpty(actionButton)) //Form Submitted!
-			{
-				page = 1;//Reset page to start
-
-				if (sortOptions.Contains(actionButton))//Change of sort is requested
-				{
-					if (actionButton == sortField) //Reverse order on same field
-					{
-						sortDirection = sortDirection == "asc" ? "desc" : "asc";
-					}
-					sortField = actionButton;//Sort by the button clicked
-				}
-			}
-			if (sortField == "Volunteer")
-			{
-				ViewData["hourVolSort"] = "fa fa-solid fa-sort";
-				ViewData["partiSort"] = "fa fa-solid fa-sort";
-				ViewData["absenceSort"] = "fa fa-solid fa-sort";
-				if (sortDirection == "asc")
-				{
-					volunteers = volunteers
-						.OrderBy(v => v.LastName)
-						.ThenBy(v => v.FirstName);
-					ViewData["volunteerSort"] = "fa fa-solid fa-sort-up";
-				}
-				else
-				{
-					volunteers = volunteers
-						.OrderByDescending(v => v.LastName)
-						.ThenBy(v => v.FirstName);
-					ViewData["volunteerSort"] = "fa fa-solid fa-sort-down";
-				}
-			}
-			else if (sortField == "Hours Volunteered")
-			{
-				ViewData["volunteerSort"] = "fa fa-solid fa-sort";
-				ViewData["partiSort"] = "fa fa-solid fa-sort";
-				ViewData["absenceSort"] = "fa fa-solid fa-sort";
-				if (sortDirection == "asc")
-				{
-					volunteers = volunteers.OrderBy(v => v.HoursVolunteered);
-					ViewData["hourVolSort"] = "fa fa-solid fa-sort-up";
-				}
-				else
-				{
-					volunteers = volunteers.OrderByDescending(v => v.HoursVolunteered);
-					ViewData["hourVolSort"] = "fa fa-solid fa-sort-down";
-				}
-			}
-			else if (sortField == "Participation")
-			{
-				ViewData["volunteerSort"] = "fa fa-solid fa-sort";
-				ViewData["hourVolSort"] = "fa fa-solid fa-sort";
-				ViewData["absenceSort"] = "fa fa-solid fa-sort";
-				if (sortDirection == "asc")
-				{
-					volunteers = volunteers.OrderBy(v => v.ParticipationCount);
-					ViewData["partiSort"] = "fa fa-solid fa-sort-up";
-				}
-				else
-				{
-					volunteers = volunteers.OrderByDescending(v => v.ParticipationCount);
-					ViewData["partiSort"] = "fa fa-solid fa-sort-down";
-				}
-			}
-			else if (sortField == "Absences")
-			{
-				ViewData["volunteersSort"] = "fa fa-solid fa-sort";
-				ViewData["hourVolSort"] = "fa fa-solid fa-sort";
-				if (sortDirection == "asc")
-				{
-					volunteers = volunteers.OrderBy(v => v.absences);
-					ViewData["absenceSort"] = "fa fa-solid fa-sort-up";
-				}
-				else
-				{
-					volunteers = volunteers.OrderByDescending(v => v.absences);
-					ViewData["absenceSort"] = "fa fa-solid fa-sort-down";
-				}
-			}
-
-
-			//Set sort for next time
-			ViewData["sortField"] = sortField;
-			ViewData["sortDirection"] = sortDirection;
-
-			//Handle Paging
-			int pageSize = PageSizeHelper.SetPageSize(HttpContext, pageSizeID, ControllerName());
-			ViewData["pageSizeID"] = PageSizeHelper.PageSizeList(pageSize);
-			var pagedData = await PaginatedList<Volunteer>.CreateAsync(volunteers.AsNoTracking(), page ?? 1, pageSize);
-
-			return View(pagedData);
-		}
-
+		
 
 
 		// GET: Volunteer
@@ -1050,8 +906,152 @@ namespace TomorrowsVoice_Toplevel.Controllers
 			};
 			return Json(data);
 		}
+        public async Task<IActionResult> IndexVolunteer(string? SearchString, int? page, int? pageSizeID, string? actionButton, string? StatusFilter, string sortField = "Volunteer", string sortDirection = "asc")
+        {
+            string[] sortOptions = new[] { "Volunteer", "Hours Volunteered", "Participation", "Absences" };
 
-		private bool VolunteerExists(int id)
+            //Count the number of filters applied - start by assuming no filters
+            ViewData["Filtering"] = "btn-outline-secondary";
+            int numberFilters = 0;
+            Enum.TryParse(StatusFilter, out Status selectedDOW);
+
+
+            var statusList = Enum.GetValues(typeof(Status))
+                         .Cast<Status>()
+                         .Where(s => s == Status.Active || s == Status.Archived)
+                         .ToList();
+
+
+            ViewBag.StatusList = new SelectList(statusList);
+            var volunteers = _context.Volunteers.AsNoTracking();
+            if (!String.IsNullOrEmpty(StatusFilter))
+            {
+                volunteers = volunteers.Where(p => p.Status == selectedDOW);
+
+                // filter out archived singers if the user does not specifically select "archived"
+                if (selectedDOW != Status.Archived)
+                {
+                    volunteers = volunteers.Where(s => s.Status != Status.Archived);
+                }
+                numberFilters++;
+            }
+            else
+            {
+                volunteers = volunteers.Where(s => s.Status != Status.Archived);
+            }
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                volunteers = volunteers.Where(p => p.LastName.ToUpper().Contains(SearchString.ToUpper())
+                                       || p.FirstName.ToUpper().Contains(SearchString.ToUpper()));
+                numberFilters++;
+            }
+            //Give feedback about the state of the filters
+            if (numberFilters != 0)
+            {
+                //Toggle the Open/Closed state of the collapse depending on if we are filtering
+                ViewData["Filtering"] = " btn-danger";
+                //Show how many filters have been applied
+                ViewData["numberFilters"] = "(" + numberFilters.ToString()
+                    + " Filter" + (numberFilters > 1 ? "s" : "") + " Applied)";
+                //Keep the Bootstrap collapse open
+                @ViewData["ShowFilter"] = " show";
+            }
+            //Before we sort, see if we have called for a change of filtering or sorting
+            if (!String.IsNullOrEmpty(actionButton)) //Form Submitted!
+            {
+                page = 1;//Reset page to start
+
+                if (sortOptions.Contains(actionButton))//Change of sort is requested
+                {
+                    if (actionButton == sortField) //Reverse order on same field
+                    {
+                        sortDirection = sortDirection == "asc" ? "desc" : "asc";
+                    }
+                    sortField = actionButton;//Sort by the button clicked
+                }
+            }
+            if (sortField == "Volunteer")
+            {
+                ViewData["hourVolSort"] = "fa fa-solid fa-sort";
+                ViewData["partiSort"] = "fa fa-solid fa-sort";
+                ViewData["absenceSort"] = "fa fa-solid fa-sort";
+                if (sortDirection == "asc")
+                {
+                    volunteers = volunteers
+                        .OrderBy(v => v.LastName)
+                        .ThenBy(v => v.FirstName);
+                    ViewData["volunteerSort"] = "fa fa-solid fa-sort-up";
+                }
+                else
+                {
+                    volunteers = volunteers
+                        .OrderByDescending(v => v.LastName)
+                        .ThenBy(v => v.FirstName);
+                    ViewData["volunteerSort"] = "fa fa-solid fa-sort-down";
+                }
+            }
+            else if (sortField == "Hours Volunteered")
+            {
+                ViewData["volunteerSort"] = "fa fa-solid fa-sort";
+                ViewData["partiSort"] = "fa fa-solid fa-sort";
+                ViewData["absenceSort"] = "fa fa-solid fa-sort";
+                if (sortDirection == "asc")
+                {
+                    volunteers = volunteers.OrderBy(v => v.HoursVolunteered);
+                    ViewData["hourVolSort"] = "fa fa-solid fa-sort-up";
+                }
+                else
+                {
+                    volunteers = volunteers.OrderByDescending(v => v.HoursVolunteered);
+                    ViewData["hourVolSort"] = "fa fa-solid fa-sort-down";
+                }
+            }
+            else if (sortField == "Participation")
+            {
+                ViewData["volunteerSort"] = "fa fa-solid fa-sort";
+                ViewData["hourVolSort"] = "fa fa-solid fa-sort";
+                ViewData["absenceSort"] = "fa fa-solid fa-sort";
+                if (sortDirection == "asc")
+                {
+                    volunteers = volunteers.OrderBy(v => v.ParticipationCount);
+                    ViewData["partiSort"] = "fa fa-solid fa-sort-up";
+                }
+                else
+                {
+                    volunteers = volunteers.OrderByDescending(v => v.ParticipationCount);
+                    ViewData["partiSort"] = "fa fa-solid fa-sort-down";
+                }
+            }
+            else if (sortField == "Absences")
+            {
+                ViewData["volunteersSort"] = "fa fa-solid fa-sort";
+                ViewData["hourVolSort"] = "fa fa-solid fa-sort";
+                if (sortDirection == "asc")
+                {
+                    volunteers = volunteers.OrderBy(v => v.absences);
+                    ViewData["absenceSort"] = "fa fa-solid fa-sort-up";
+                }
+                else
+                {
+                    volunteers = volunteers.OrderByDescending(v => v.absences);
+                    ViewData["absenceSort"] = "fa fa-solid fa-sort-down";
+                }
+            }
+
+
+            //Set sort for next time
+            ViewData["sortField"] = sortField;
+            ViewData["sortDirection"] = sortDirection;
+
+            //Handle Paging
+            int pageSize = PageSizeHelper.SetPageSize(HttpContext, pageSizeID, ControllerName());
+            ViewData["pageSizeID"] = PageSizeHelper.PageSizeList(pageSize);
+            var pagedData = await PaginatedList<Volunteer>.CreateAsync(volunteers.AsNoTracking(), page ?? 1, pageSize);
+
+            return View(pagedData);
+        }
+
+        private bool VolunteerExists(int id)
 		{
 			return _context.Volunteers.Any(e => e.ID == id);
 		}
