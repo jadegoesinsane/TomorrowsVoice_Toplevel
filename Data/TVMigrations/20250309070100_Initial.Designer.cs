d0d9088f19aa69dff8f8b313154117c99cd5aa2d
cheng1111111111111 <tli15@ncstudents.niagaracollege.ca>
2025-03-09 07:11:09 -0400

    can not join passed shift

--- Data/TVMigrations/20250309070100_Initial.Designer.cs
+++ Data/TVMigrations/20250309070100_Initial.Designer.cs
@@ -11,7 +11,7 @@ using TomorrowsVoice_Toplevel.Data;
 namespace TomorrowsVoice_Toplevel.Data.TVMigrations
 {
     [DbContext(typeof(TVContext))]
-    [Migration("20250309044328_Initial")]
+    [Migration("20250309070100_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
