daf008d17d9864e136b61af281906818ad7db8b7
jadegoesinsane <jkritzer1@ncstudents.niagaracollege.ca>
2025-03-19 12:42:21 -0400

    Add Director Account

---
+++ Controllers/DirectorAccountController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TomorrowsVoice_Toplevel.CustomControllers;
+using TomorrowsVoice_Toplevel.Data;
+using TomorrowsVoice_Toplevel.Models.Events;
+using TomorrowsVoice_Toplevel.Utilities;
+using TomorrowsVoice_Toplevel.ViewModels;
+
+namespace TomorrowsVoice_Toplevel.Controllers
+{
+	/// <summary>
+	/// Specialized controller just used to allow an
+	/// Authenticated user to maintain their own account details.
+	/// </summary>
+	[Authorize]
+	public class DirectorAccountController : CognizantController
+	{
+		private readonly TVContext _context;
+
+		public DirectorAccountController(TVContext context)
+		{
+			_context = context;
+		}
+
+		// GET: DirectorAccount
+		public IActionResult Index()
+		{
+			return RedirectToAction(nameof(Details));
+		}
+
+		// GET: DirectorAccount/Details/5
+		public async Task<IActionResult> Details()
+		{
+			var director = await _context.Directors
+			   .Where(d => d.Email == User.Identity.Name)
+			   .Select(d => new DirectorVM
+			   {
+				   ID = d.ID,
+				   FirstName = d.FirstName,
+				   MiddleName = d.MiddleName,
+				   LastName = d.LastName,
+				   Phone = d.Phone
+			   })
+			   .FirstOrDefaultAsync();
+			if (director == null)
+			{
+				return NotFound();
+			}
+
+			return View(director);
+		}
+
+		// GET: DirectorAccount/Edit/5
+		public async Task<IActionResult> Edit()
+		{
+			var director = await _context.Directors
+			   .Where(d => d.Email == User.Identity.Name)
+			   .Select(d => new DirectorVM
+			   {
+				   ID = d.ID,
+				   FirstName = d.FirstName,
+				   MiddleName = d.MiddleName,
+				   LastName = d.LastName,
+				   Phone = d.Phone
+			   })
+			   .FirstOrDefaultAsync();
+			if (director == null)
+			{
+				return NotFound();
+			}
+			return View(director);
+		}
+
+		// POST: DirectorAccount/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id)
+		{
+			var directorToUpdate = await _context.Directors
+				.FirstOrDefaultAsync(m => m.ID == id);
+
+			//Note: Using TryUpdateModel we do not need to invoke the ViewModel
+			//Only allow some properties to be updated
+			if (await TryUpdateModelAsync<Director>(directorToUpdate, "",
+				c => c.FirstName, c => c.MiddleName, c => c.LastName, c => c.Phone))
+			{
+				try
+				{
+					_context.Update(directorToUpdate);
+					await _context.SaveChangesAsync();
+					UpdateUserNameCookie(directorToUpdate.NameFormatted);
+					return RedirectToAction(nameof(Details));
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!DirectorExists(directorToUpdate.ID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				catch (DbUpdateException)
+				{
+					//Since we do not allow changing the email, we cannot introduce a duplicate
+					ModelState.AddModelError("", "Something went wrong in the database.");
+				}
+			}
+			return View(directorToUpdate);
+		}
+
+		private void UpdateUserNameCookie(string userName)
+		{
+			CookieHelper.CookieSet(HttpContext, "userName", userName, 960);
+		}
+
+		private bool DirectorExists(int id)
+		{
+			return _context.Directors.Any(e => e.ID == id);
+		}
+	}
+}
---
+++ ViewModels/DirectorVM.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TomorrowsVoice_Toplevel.Models.Events;
+using TomorrowsVoice_Toplevel.Models.Volunteering;
+
+namespace TomorrowsVoice_Toplevel.ViewModels
+{
+	[ModelMetadataType(typeof(DirectorMetaData))]
+	public class DirectorVM
+	{
+		public int ID { get; set; }
+
+		#region Person Data
+
+		public string NameFormatted => FirstName + (string.IsNullOrEmpty(MiddleName) ? " "
+			: (" " + (char?)MiddleName[0] + ". ").ToUpper()) + LastName;
+
+		public string PhoneFormatted => "(" + Phone?.Substring(0, 3) + ") "
+			+ Phone?.Substring(3, 3) + "-" + Phone?[6..];
+
+		public string FirstName { get; set; } = "";
+		public string? MiddleName { get; set; }
+		public string LastName { get; set; } = "";
+		public virtual string Phone { get; set; } = "";
+
+		#endregion Person Data
+
+		#region Director Data
+
+		public virtual ICollection<Rehearsal> Rehearsals { get; set; } = new HashSet<Rehearsal>();
+		public ICollection<DirectorDocument> Documents { get; set; } = new HashSet<DirectorDocument>();
+
+		#endregion Director Data
+	}
+}
